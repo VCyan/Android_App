1b7ee192cb339968572bcd2bb8ecbcbea8dfe1bf
VCyan <martinvictor45@hotmail.com>
2018-12-01 20:08:05 -0600

    Preloaded insert user OK
    Working Project
    
    Signed-off-by: VCyan <martinvictor45@hotmail.com>

--- app/src/main/java/example/gabo/com/testapp/MainActivity.java
+++ app/src/main/java/example/gabo/com/testapp/MainActivity.java
@@ -20,34 +20,36 @@ import android.widget.EditText;
 import example.gabo.com.testapp.DatabaseActivity;
 
 public class MainActivity extends AppCompatActivity implements MenuFragment.OnFragmentInteractionListener{
+    int attempt_counter=5;
+    FishDatabaseController controller;
+    public static String MESSAGE_ID = "com.example.fly.testApp.message";
+    //creacion del fragmento
+    MenuFragment menuFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //IntentFilter filter = new IntentFilter(ReminderService.ACTION_NOTIFICATION);
+        //LocalBroadcastManager.getInstance(this).registerReceiver( new MainBroadcastReceiver(this, filter) );
+        controller = new FishDatabaseController(this.getBaseContext());
+        //Creacion del fragmento
+        //menuFragment = new MenuFragment();
+        //FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        //ft.add(R.id.fragmentContainer,menuFragment);
+        //ft.commit();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        controller.close();
+    }
 
     @Override
     public void onFragmentInteraction(Uri uri){
 
 
-    }
-
-    FishDatabaseController controller;
-
-    public static String MESSAGE_ID = "com.example.fly.testApp.message";
-
-    //creacion del fragmento
-    MenuFragment menuFragment;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        //IntentFilter filter = new IntentFilter(ReminderService.ACTION_NOTIFICATION);
-        //LocalBroadcastManager.getInstance(this).registerReceiver( new MainBroadcastReceiver(this, filter) );
-
-        controller = new FishDatabaseController(this.getBaseContext());
-        //Creacion del fragmento
-        //menuFragment = new MenuFragment();
-        //FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-
-        //ft.add(R.id.fragmentContainer,menuFragment);
-        //ft.commit();
     }
 
     public void buttonClicked(View view){
@@ -69,18 +71,21 @@ public class MainActivity extends AppCompatActivity implements MenuFragment.OnFr
     }
 
     public void loginAction(View view){
-
         Log.d("Database App", "Login check");
         UserEntry usr = new UserEntry();
-        EditText myText = (EditText)findViewById(R.id.editText_Username);
+        EditText myText = findViewById(R.id.editText_Username);
         EditText pwd = findViewById(R.id.editText_Password);
+
         usr.setUser(myText.getText().toString());
         usr.setPassword(pwd.getText().toString());
+
         String[] args ={usr.getUser(),usr.getPassword()};
-        Cursor cursor = controller.selectUsers(UserEntry.COLUMN_PASS+"=? AND "+UserEntry.COLUMN_PASS+"=?", args);
-        if(!cursor.equals(null)) {
+        Cursor cursor = controller.selectUsers(UserEntry.COLUMN_USER+"=? AND "+UserEntry.COLUMN_PASS+"=?", args);
+
+        if(cursor!=null && cursor.getCount()>0) {
             Log.d("Login", "Valid");
-            Intent newActivity = new Intent(this, LockScreen.class); //cambia la screen a la de databaseScreen
+
+            Intent newActivity = new Intent(this, LockScreen.class);
             //String value = ""+myText.getText();
             //newActivity.putExtra(MESSAGE_ID, value);
             startActivity(newActivity);
